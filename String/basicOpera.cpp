1ad483b91f16abfb98a0d2b5e3ba8b1cdeefbe45
Soumik Mondal <soumikmondal1727@gmail.com>
2024-08-24 02:35:36 +0530

    basic string

---
+++ String/basicOpera.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include<typeinfo>
+
+using namespace std;
+
+int main() {
+    // Initialize a string with a single character
+    string s = "1";
+    cout << typeid(s[0]).name() << endl;        // char
+    cout << int(s[0]) << endl;                  // 49
+    cout << typeid(s[0] - '0').name() << endl;  // int
+    cout << int(s[0] - '0') << endl;            // 1
+    cout << char((s[0] - '0') + '0') << endl;   // '1'
+    cout << typeid(char((s[0] - '0') + '0')).name();     // char
+
+    int ascii = 50;
+    cout <<char(ascii) << endl;     // 2
+
+    return 0;
+}
