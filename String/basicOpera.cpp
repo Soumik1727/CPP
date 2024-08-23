f4c66c83f07063d7b53623fe954e84c59c028a9e
Soumik Mondal <soumikmondal1727@gmail.com>
2024-08-24 03:02:54 +0530

    fdggdf

--- String/basicOpera.cpp
+++ String/basicOpera.cpp
@@ -5,18 +5,19 @@
 using namespace std;
 
 int main() {
-    // Initialize a string with a single character
+    
     string s = "1";
-    cout << typeid(s[0]).name() << endl;        // char
+
     cout << int(s[0]) << endl;                  // 49
     cout << typeid(s[0] - '0').name() << endl;  // int
+
+    cout << (s[0] - '0')<< endl;                   // 1
     cout << int(s[0] - '0') << endl;            // 1
-    cout << char((s[0] - '0') + '0') << endl;   // '1'
-    cout << typeid(char((s[0] - '0') + '0')).name() << endl;     // char
-    cout << "(s[0] - '0'): " << (s[0] - '0')<< endl;
-    cout << "Type: " << typeid((s[0] - '0')).name() << endl;
-    int ascii = 50;
-    cout <<char(ascii) << endl;     // 2
+    
+    int number = 50;
+    cout <<char(number) << endl;     // 2
+    string res = to_string(number);
+    cout << res << endl;            // 50
 
     return 0;
 }
